cdcc4996c309c84f3845ba0a6a9031db57cd3151
Shaun Williams <shaun.e.williams@gmail.com>
2017-07-23 18:16:45 -0500

    add example code to demo

--- demo/index.js
+++ demo/index.js
@@ -1,4 +1,14 @@
 
 var cm = CodeMirror(document.body);
 
+cm.setValue(`
+(defn foo
+  "hello, this is a docstring"
+  [a b]
+  (let [sum (+ a b)
+        prod (* a b)]
+     {:sum sum
+      :prod prod}))
+`);
+
 activate(cm);
