7614590b73d3ddc935e437dc79ee02c3bbe21fc3
Shaun Williams <shaun.e.williams@gmail.com>
2017-07-23 23:06:35 -0500

    build public API

--- README.md
+++ README.md
@@ -1,8 +1,52 @@
+> __NOTE__ work in progress
+
 # Parinfer for CodeMirror
 
-The original implementation used for the [Parinfer website] was rushed for the
-sake of proving the idea in an animated way.  This will simplify the implementation,
-remove the custom internal hacks, and improve the integration required by new
-Parinfer features.
+A [Parinfer] layer for the browser-based [CodeMirror] editor.
 
-[Parinfer website]:http://shaunlebron.github.io/parinfer/
+It is intended to be the canonical editor for demonstrating the behavior of
+Parinfer in its official online [demo editor]. (It is currently not using this
+implementation yet)
+
+[demo editor]:http://shaunlebron.github.io/parinfer/demo
+
+## Usage
+
+Attach Parinfer to a CodeMirror instance.  See [demo.html] for working example.
+
+[demo.html]:demo.html
+
+```js
+parinferCodeMirror.init(cm);
+```
+
+__NOTE__: If the CodeMirror editor has unbalanced code when running `init`, the
+editor will be suspended in a "correction" mode (i.e. Paren Mode) until the
+highlighted errors are fixed—after which the editor enters the originally
+intended mode as expected.
+
+## API
+
+```js
+parinferCodeMirror.init(cm, mode, options);
+// `mode` is 'paren', 'indent', or 'smart'
+// `options` is passed to Parinfer
+
+parinferCodeMirror.enable(cm);
+parinferCodeMirror.disable(cm);
+parinferCodeMirror.setMode(cm, mode);
+parinferCodeMirror.setOptions(cm, options);
+```
+
+All of the API functions above will return a boolean indicating if the text was
+successfully processed without errors. (`disable` not included)
+
+## History
+
+The original implementation used for the [Parinfer] website was rushed for the
+sake of proving the idea in an animated way.  This standalone version simplifies
+the implementation, removes the custom internal hacks, and improves the
+integration required by new Parinfer features.
+
+[Parinfer]:http://shaunlebron.github.io/parinfer/
+[CodeMirror]:http://codemirror.net/
---
+++ parinfer-codemirror.js
@@ -0,0 +1,302 @@
+//
+// Parinfer for CodeMirror 1.0.0
+//
+// Copyright 2017 © Shaun Lebron
+// MIT License
+//
+
+//------------------------------------------------------------------------------
+// JS Module Boilerplate
+//------------------------------------------------------------------------------
+
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  }
+  else if (typeof module === 'object' && module.exports) {
+    module.exports = factory();
+  }
+  else {
+    root.parinferCodeMirror = factory();
+  }
+}(this, function() { // start module anonymous scope
+"use strict";
+
+//------------------------------------------------------------------------------
+// Constants
+//------------------------------------------------------------------------------
+
+// We attach our Parinfer state to this property on the CodeMirror instance.
+var STATE_PROP = '__parinfer__';
+
+var PAREN_MODE = 'paren';
+var INDENT_MODE = 'indent';
+var SMART_MODE = 'smart';
+
+var MODES = [PAREN_MODE, INDENT_MODE, SMART_MODE];
+
+var ERROR_CLASSNAME = 'parinfer-error';
+
+//------------------------------------------------------------------------------
+// Errors
+//------------------------------------------------------------------------------
+
+function error(msg) {
+  return 'parinferCodeMirror: ' + msg;
+}
+
+function ensureMode(mode) {
+  if (MODES.indexOf(mode) === -1) {
+    throw error(
+      'Mode "' + mode + '" is invalid. ' +
+      'Must be one of: ' + MODES.join(',')
+    );
+  }
+}
+
+function ensureState(cm) {
+  var state = cm[STATE_PROP];
+  if (!state) {
+    throw error(
+      'You must call parinferCodeMirror.init(cm) on a CodeMirror instance ' +
+      'before you can use the rest of the API.'
+    );
+  }
+  return state;
+}
+
+//------------------------------------------------------------------------------
+// Stateless CodeMirror helpers
+//------------------------------------------------------------------------------
+
+function convertChanges(changes) {
+  return changes.map(function(change) {
+    return {
+      x: change.from.ch,
+      lineNo: change.from.line,
+      oldText: change.removed.join('\n'),
+      newText: change.text.join('\n')
+    };
+  });
+}
+
+function clearErrorMarks(cm) {
+  var i;
+  var marks = cm.getAllMarks();
+  for (i=0; i<marks.length; i++) {
+    if (marks[i].className === ERROR_CLASSNAME) {
+      marks[i].clear();
+    }
+  }
+}
+
+function addErrorMark(cm, lineNo, x) {
+  var from = {line: lineNo, ch: x};
+  var to = {line: lineNo, ch: x+1};
+  cm.markText(from, to, {className: ERROR_CLASSNAME});
+}
+
+function updateErrorMarks(cm, error) {
+  clearErrorMarks(cm);
+  if (error) {
+    addErrorMark(cm, error.lineNo, error.x);
+    if (error.extra) {
+      addErrorMark(cm, error.extra.lineNo, error.extra.x);
+    }
+  }
+}
+
+//------------------------------------------------------------------------------
+// Stateful CodeMirror functions
+// (`state` represents the parinfer state of a single CodeMirror editor)
+//------------------------------------------------------------------------------
+
+function initialState(cm, mode, options) {
+  return {
+    cm: cm,
+    mode: mode,
+    options: options,
+    enabled: false,
+    cursorTimeout: null,
+    monitorCursor: true,
+    prevCursorX: null,
+    prevCursorLine: null,
+    callbackCursor: null,
+    callbackChanges: null,
+  };
+}
+
+function onCursorChange(state) {
+  clearTimeout(state.cursorTimeout);
+  if (state.monitorCursor) {
+    state.cursorTimeout = setTimeout(function () { fixText(state); }, 0);
+  }
+}
+
+function onTextChanges(state, changes) {
+  if (changes[0].origin !== 'setValue') {
+    clearTimeout(state.cursorTimeout);
+    fixText(state, changes);
+  }
+}
+
+// If `changes` is missing, then only the cursor position has changed.
+function fixText(state, changes) {
+  // Get editor data
+  var cm = state.cm;
+  var text = cm.getValue();
+  var hasSelection = cm.somethingSelected();
+  var selections = cm.listSelections();
+  var cursor = cm.getCursor();
+  var scroller = cm.getScrollerElement();
+
+  // Create options
+  var options = {
+    cursorLine: cursor.line,
+    cursorX: cursor.ch,
+    prevCursorLine: state.prevCursorLine,
+    prevCursorX: state.prevCursorX
+  };
+  if (state.options) {
+    var p;
+    for (p in state.options) {
+      if (state.options.hasOwnProperty(p)) {
+        options[p] = state.options[p];
+      }
+    }
+  }
+  if (changes) {
+    options.changes = convertChanges(changes);
+  }
+
+  // Run Parinfer
+  var result;
+  var mode = state.fixMode ? PAREN_MODE : state.mode;
+  switch (mode) {
+    case INDENT_MODE: result = parinfer.indentMode(text, options); break;
+    case PAREN_MODE:  result = parinfer.parenMode(text, options); break;
+    case SMART_MODE:  result = parinfer.smartMode(text, options); break;
+    default: ensureMode(mode);
+  }
+
+  // Clear or add new error marks
+  updateErrorMarks(cm, result.error);
+
+  if (text !== result.text) {
+    // Update text
+    cm.setValue(result.text);
+
+    // Update cursor and selection
+    state.monitorCursor = false;
+    if (hasSelection) {
+      cm.setSelections(selections);
+    } else {
+      cm.setCursor(result.cursorLine, result.cursorX);
+    }
+    setTimeout(function(){ state.monitorCursor = true; }, 0);
+
+    // Update scroll position
+    cm.scrollTo(scroller.scrollLeft, scroller.scrollTop);
+  }
+
+  // Remember the cursor position for next time
+  state.prevCursorLine = result.cursorLine;
+  state.prevCursorX = result.cursorX;
+
+  // Re-run with original mode if code was finally fixed in Paren Mode.
+  if (state.fixMode && result.success) {
+    state.fixMode = false;
+    return fixText(state, changes);
+  }
+
+  return result.success;
+}
+
+//------------------------------------------------------------------------------
+// CodeMirror Integration
+//------------------------------------------------------------------------------
+
+function on(state) {
+  if (state.enabled) {
+    return;
+  }
+  state.callbackCursor = function(cm) {
+    onCursorChange(state);
+  };
+  state.callbackChanges = function(cm, changes) {
+    onTextChanges(state, changes);
+  };
+  cm.on('cursorActivity', state.callbackCursor);
+  cm.on('changes', state.callbackChanges);
+  state.enabled = true;
+}
+
+function off(state) {
+  if (!state.enabled) {
+    return;
+  }
+  clearErrorMarks(state.cm);
+  cm.off('cursorActivity', state.callbackCursor);
+  cm.off('changes', state.callbackChanges);
+  state.enabled = false;
+}
+
+//------------------------------------------------------------------------------
+// Public API
+//------------------------------------------------------------------------------
+
+function init(cm, mode, options) {
+  var state = cm[STATE_PROP];
+  if (state) {
+    throw error('init has already been called on this CodeMirror instance');
+  }
+
+  mode = mode || SMART_MODE;
+  ensureMode(mode);
+
+  state = initialState(cm, mode, options);
+  cm[STATE_PROP] = state;
+  return enable(cm);
+}
+
+function enable(cm) {
+  var state = ensureState(cm);
+
+  // preprocess text to keep Parinfer from changing code structure
+  if (state.mode !== PAREN_MODE) {
+    state.fixMode = true;
+  }
+
+  on(state);
+  return fixText(state);
+}
+
+function disable(cm) {
+  var state = ensureState(cm);
+  off(state);
+}
+
+function setMode(cm, mode) {
+  var state = ensureState(cm);
+  ensureMode(mode);
+  state.mode = mode;
+  return fixText(state);
+}
+
+function setOptions(state, options) {
+  state.options = options;
+  return fixText(state);
+}
+
+var API = {
+  version: "1.0.0",
+  init: init,
+  enable: enable,
+  disable: disable,
+  setMode: setMode,
+  setOptions: setOptions
+};
+
+return API;
+
+})); // end module anonymous scope
