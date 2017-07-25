343a4b29be90dac911f643da1a908fed96dbf242
Shaun Williams <shaun.e.williams@gmail.com>
2017-07-25 12:30:54 -0500

    add version sync script

---
+++ sync.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+jsfile=parinfer-codemirror.js
+
+##----------------------------------------------------------------------------
+## Version sync
+##----------------------------------------------------------------------------
+
+# Get package.json version.
+version=$(perl -n -e'/"version": "(.+)"/ && print "$1"' package.json)
+
+# Sync version to code files.
+sed -i.bak "s|^// Parinfer .*|// Parinfer $version|" $jsfile
+sed -i.bak "s|^  version: .*|  version: \"$version\",|" $jsfile
+
+rm ${jsfile}.bak
+
+echo "Updated $jsfile with package.json version $version"
+echo
